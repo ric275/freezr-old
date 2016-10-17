7c0bdc8d55052bd24ea13af31ee3c5d34125ac76
Jack Taylor <ajacktaylor@me.com>
2016-10-17 23:48:32 +0100

    More work

--- Freezr/AboutViewController.swift
+++ Freezr/AboutViewController.swift
@@ -14,21 +14,52 @@ class AboutViewController: UIViewController {
     
     let myPurple:UIColor = UIColor(red: 105/255.0, green: 94/255.0, blue: 133/255.0, alpha: 1.0)
     
-    //Outlets
+    //Outlets - loads of label outlets because I'm too lazy to change the text colour in the storyboard.
     
     @IBOutlet var aboutView: UIView!
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    
+    @IBOutlet weak var ackButton: UIButton!
+    
+    //Variables
+    
+    var tapCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        aboutView.backgroundColor = myPurple
+        label1.textColor = UIColor.white
+        label2.textColor = UIColor.white
+        label3.textColor = UIColor.white
+        label4.textColor = UIColor.white
+        label5.textColor = UIColor.white
+        ackButton.tintColor = UIColor.white
+        
+        
+    }
     
+    @IBAction func ackButtonTapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func raspberryTapped(_ sender: AnyObject) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount == 7 {
+            label1.text = "meow"
+            
+        } else {}
         
     }
     
     
-    
-
     //Final declaration:
-
+    
 }
