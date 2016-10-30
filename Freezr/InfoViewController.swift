8c959299397a48cc4153cce8cd7955b21d01f1c3
Jack Taylor <ajacktaylor@me.com>
2016-10-30 16:40:23 +0000

    More or less finished the Settings page.

--- Freezr/InfoViewController.swift
+++ Freezr/InfoViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import MessageUI
+import AVKit
+import AVFoundation
 
-class InfoViewController: UITableViewController {
+class InfoViewController: UITableViewController, MFMailComposeViewControllerDelegate {
     
+    //Custom colours.
+    
+    let myPurple:UIColor = UIColor(red: 105/255.0, green: 94/255.0, blue: 133/255.0, alpha: 1.0)
+    
+    var playerViewController = AVPlayerViewController()
+    var playerView = AVPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let urlPathString:String? = Bundle.main.path(forResource: "Freezr Demo", ofType: "mp4")
+        
+        if let urlPath = urlPathString {
+            
+            let fileURL = NSURL(fileURLWithPath: urlPath)
+            
+            playerView = AVPlayer(url: fileURL as URL)
+            playerViewController.player = playerView
+        }
     }
     
     
@@ -37,25 +56,31 @@ class InfoViewController: UITableViewController {
         let feedbackCell = UITableViewCell(style: .value1, reuseIdentifier: nil)
         let versionCell = UITableViewCell(style: .value1, reuseIdentifier: nil)
         
-        howCell.textLabel?.text = "How to use"
+        howCell.textLabel?.text = "Video: How to use Freezr"
+        howCell.textLabel?.textColor = .purple
         howCell.accessoryType =  .disclosureIndicator
         
         aboutCell.textLabel?.text = "About Freezr"
+        aboutCell.textLabel?.textColor = .purple
         aboutCell.accessoryType =  .disclosureIndicator
         
         feedbackCell.textLabel?.text = "Send Feedback"
+        feedbackCell.textLabel?.textColor = .purple
         feedbackCell.accessoryType =  .disclosureIndicator
         
         versionCell.textLabel?.text = "App Version:"
+        versionCell.textLabel?.textColor = .purple
         if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
-            versionCell.detailTextLabel?.text = "\(version)" }
+            versionCell.detailTextLabel?.text = "\(version)"
+            versionCell.detailTextLabel?.textColor = myPurple
+        }
         
         if indexPath.section == 0 {
             return howCell
         } else if indexPath.section == 1 {
             
             if indexPath.row == 0 {
-            return aboutCell
+                return aboutCell
             } else {
                 return feedbackCell
             }
@@ -70,12 +95,56 @@ class InfoViewController: UITableViewController {
             if indexPath.row == 0 {
                 performSegue(withIdentifier: "aboutSegue", sender: nil)
                 
+            } else {
+                let mailComposeViewController = self.configuredMailComposeViewController()
+                if MFMailComposeViewController.canSendMail() {
+                    self.present(mailComposeViewController, animated: true, completion: nil)
+                    
+                } else {
+                    self.showSendMailErrorAlert()
+                }
+            }
+        } else if indexPath.section == 0 {
+            self.present(playerViewController, animated: true) {
+                
+                self.playerViewController.player?.play()
+                
             }
         }
         
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //Setup mail feeback.
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["jack@blueinkcode.com"])
+        mailComposerVC.setSubject("Freezr Feedback")
+        mailComposerVC.setMessageBody("*You can use this email to inform us of any bugs or let us know about any feature requests.\n", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    //Setup mail alerts.
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertController(title: "Well this sucks.", message: "We couldn't prepare your email. Please check your mail settings and then try again.", preferredStyle: .alert)
+        let cont = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        sendMailErrorAlert.addAction(cont)
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+    }
+    
+    //Dismiss the mail ViewController.
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     
     
     
