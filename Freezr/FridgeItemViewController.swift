46296c87aa68786f22e9aca6eab00a17755075ee
Jack Taylor <ajacktaylor@me.com>
2016-11-02 00:26:58 +0000

    Started work on Fridge.

---
+++ Freezr/FridgeItemViewController.swift
@@ -0,0 +1,273 @@
+//
+//  FridgeItemViewController.swift
+//  Freezr
+//
+//  Created by Jack Taylor on 01/11/2016.
+//  Copyright © 2016 Jack Taylor. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class FridgeItemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    //Custom colours.
+    
+    let myPurple:UIColor = UIColor(red: 105/255.0, green: 94/255.0, blue: 133/255.0, alpha: 1.0)
+    
+    //Outlets.
+    
+    @IBOutlet weak var itemImage: UIImageView!
+    
+    @IBOutlet weak var itemName: UITextField!
+    
+    @IBOutlet weak var addItemOrUpdateButton: UIButton!
+    
+    @IBOutlet weak var deleteItemButton: UIButton!
+    
+    @IBOutlet weak var expirationDateTextField: UITextField!
+    
+    @IBOutlet weak var placeHolderText1: UILabel!
+    
+    @IBOutlet weak var placeHolderText2: UILabel!
+    
+    @IBOutlet weak var addToSLButton: UIButton!
+    
+    @IBOutlet weak var imageNoticeText: UILabel!
+    
+    @IBOutlet weak var expiresLabel: UILabel!
+    
+    //Variables.
+    
+    var imageSelector = UIImagePickerController()
+    
+    var fridgeItem : FridgeItem? = nil
+    
+    let today = NSDate()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        imageSelector.delegate = self
+        
+        itemName.delegate = self
+        
+        itemName.returnKeyType = UIReturnKeyType.done
+        
+        itemName.textColor = myPurple
+        
+        expirationDateTextField.textColor = myPurple
+        
+        //Setup the item view depending on if an existing item is being selected, or a new item is being added.
+        
+        //If there is an existing item:
+        
+        if fridgeItem != nil {
+            itemImage.image = UIImage(data: fridgeItem!.image as! Data)
+            itemName.text = fridgeItem!.name
+            expirationDateTextField.text = fridgeItem!.expirydate
+            
+            placeHolderText1.isHidden = true
+            placeHolderText2.isHidden = true
+            imageNoticeText.isHidden = true
+            
+            addItemOrUpdateButton.setTitle("Update item", for: .normal)
+            
+            //Expiry text setup.
+            
+            // Convert the String to a NSDate.
+            
+            let dateString = expirationDateTextField.text
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd/MMM/yyyy"
+            //dateFormatter.timeZone = TimeZone(abbreviation: "GMT+0:00")
+            let dateFromString = dateFormatter.date(from: dateString!)
+            
+            //Change expiry text and colour accordingly.
+            
+            if (fridgeItem?.expirydate?.isEmpty)! {
+                
+                print("No expiry date given - do nothing")
+                
+            } else {
+                
+                if today.isGreaterThanDate(dateToCompare: dateFromString!) {
+                    expirationDateTextField.textColor = .red
+                    expiresLabel.text = "Expired:"
+                    expiresLabel.textColor = .red
+                    
+                } else {}
+                
+            }
+            
+            //If there is not existing item:
+            
+        } else {
+            
+            deleteItemButton.isHidden = true
+            addToSLButton.isHidden = true
+            addItemOrUpdateButton.isEnabled = false
+        }
+        
+        //Dismiss the keyboard when tapped away (setup).
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ItemViewController.dismissKeyboard)))
+        
+    }
+    
+    //Select image methods.
+    
+    @IBAction func photosTapped(_ sender: AnyObject) {
+        imageSelector.sourceType = .photoLibrary
+        present(imageSelector, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+        imageSelector.sourceType = .camera
+        present(imageSelector, animated: true, completion: nil)
+    }
+    
+    //Method called when an image has been selected.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        itemImage.image = image
+        
+        imageSelector.dismiss(animated: true, completion: nil)
+        
+        placeHolderText1.isHidden = true
+        placeHolderText2.isHidden = true
+        imageNoticeText.isHidden = true
+        
+        addItemOrUpdateButton.isEnabled = true
+    }
+    
+    //What happens when Add or Update is tapped.
+    
+    @IBAction func addToFridgeTapped(_ sender: Any) {
+
+        //If updating an exisitng item.
+        
+        if fridgeItem != nil {
+            fridgeItem!.name = itemName.text
+            fridgeItem!.image = UIImageJPEGRepresentation(itemImage.image!, 0.05)! as NSData?
+            fridgeItem?.expirydate = expirationDateTextField.text
+            
+            //If creating a new item.
+            
+        } else {
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let item = FridgeItem(context: context)
+            item.name = itemName.text
+            item.image = UIImageJPEGRepresentation(itemImage.image!, 0.05)! as NSData? //was 0.1
+            item.expirydate = expirationDateTextField.text
+        }
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+    }
+    
+    //What happens when delete is tapped.
+    
+    @IBAction func deleteItemTapped(_ sender: AnyObject) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        context.delete(fridgeItem!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+    }
+    
+    //Expiry date setup
+    
+    @IBAction func editingExpirationDateTextField(_ sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.date
+        
+        sender.inputView = datePickerView
+        
+        datePickerView.addTarget(self, action: #selector(ItemViewController.datePickerValueChanged), for: UIControlEvents.valueChanged)
+    }
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        
+        //Sets it so the entered date is always UK regardless of region so the app doesn't crash when converting the date.
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_GB") as Locale
+        
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        
+        dateFormatter.timeStyle = DateFormatter.Style.none
+        
+        expirationDateTextField.text = dateFormatter.string(from: sender.date)
+        
+        //Send data to the notification func in delegate.
+        let selectedDate = sender.date
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        delegate?.scheduleNotification(at: selectedDate)
+        
+        print("Selected date: \(selectedDate)")
+    }
+    
+    //What happens when Add to Shopping List is tapped.
+    
+    @IBAction func addToSLTapped(_ sender: AnyObject) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let SLItem = ShoppingListItem(context: context)
+        SLItem.name = itemName.text
+        SLItem.image = UIImageJPEGRepresentation(itemImage.image!, 0.05)! as NSData?
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        //Add badge to SL icon.
+        
+        for item in self.tabBarController!.tabBar.items! {
+            if item.title == "Shopping List" {
+                item.badgeValue = "New!"
+            }
+        }
+        
+        navigationController!.popViewController(animated: true)
+    }
+    
+    //Dismiss the keyboard functions.
+    
+    func dismissKeyboard() {
+        
+        //Dismiss the keyboard.
+        itemName.resignFirstResponder()
+        
+        //Dismiss date picker.
+        expirationDateTextField.resignFirstResponder()
+    }
+    
+    //Dismiss the keyboard when return is tapped.
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        itemName.resignFirstResponder()
+        return true
+    }
+    
+    //Orientation setup.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let value = UIInterfaceOrientation.portrait.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+        
+        let appdelegate = UIApplication.shared.delegate as! AppDelegate
+        appdelegate.shouldSupportAllOrientation = false
+    }
+    
+    //Final declaration:
+    
+}
---
+++ Freezr/FridgeViewController.swift
@@ -0,0 +1,245 @@
+//
+//  FridgeViewController.swift
+//  Freezr
+//
+//  Created by Jack Taylor on 01/11/2016.
+//  Copyright © 2016 Jack Taylor. All rights reserved.
+//
+
+import UIKit
+
+class FridgeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //Outlets.
+    
+    @IBOutlet weak var itemListTableView: UITableView!
+    
+    @IBOutlet weak var emptyMessage1: UILabel!
+    
+    @IBOutlet weak var emptyMessage2: UILabel!
+    
+    //Variables.
+    
+    var fridgeItems : [FridgeItem] = []
+    
+    let today = NSDate()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        itemListTableView.dataSource = self
+        itemListTableView.delegate = self
+        
+    }
+    
+    //Custom colours.
+    
+    let myPurple:UIColor = UIColor(red: 105/255.0, green: 94/255.0, blue: 133/255.0, alpha: 1.0)
+    
+    //Retrieve the FridgeItems from CoreData.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            fridgeItems = try context.fetch(FridgeItem.fetchRequest())
+            itemListTableView.reloadData()
+        } catch {}
+        
+        //Orientation setup.
+        
+        let appdelegate = UIApplication.shared.delegate as! AppDelegate
+        appdelegate.shouldSupportAllOrientation = true
+    }
+    
+    //Specifies how many rows are in the table.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        if fridgeItems.count == 0 {
+            return 1
+        } else {
+            return fridgeItems.count
+        }
+    }
+    
+    //Specifies what goes in the table cells.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
+        
+        if fridgeItems.count == 0 {
+            cell.textLabel?.text = "You should probably go buy food."
+        } else {
+            let fridgeItem = fridgeItems[indexPath.row]
+            cell.textLabel?.text = fridgeItem.name
+            cell.imageView?.image = UIImage(data: fridgeItem.image as! Data)
+            
+            if (fridgeItem.expirydate?.isEmpty)! {
+                cell.textLabel?.textColor = myPurple
+                cell.detailTextLabel?.text = "Expires: Unknown"
+                
+            } else {
+                
+                //Expiry text setup.
+                
+                // Convert the String to a NSDate.
+                
+                let dateString = fridgeItem.expirydate
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "dd/MMM/yyyy"
+                //dateFormatter.timeZone = TimeZone(abbreviation: "GMT+0:00")
+                let dateFromString = dateFormatter.date(from: dateString!)
+                
+                //Change expiry text and colour accordingly.
+                
+                if today.isGreaterThanDate(dateToCompare: dateFromString!) {
+                    cell.textLabel?.textColor = .red
+                    cell.detailTextLabel?.text = "Expired: \(fridgeItem.expirydate!)"
+                    cell.detailTextLabel?.textColor = .red
+                    
+                } else {
+                    
+                    cell.textLabel?.textColor = myPurple
+                    cell.detailTextLabel?.text = "Expires: \(fridgeItem.expirydate!)"
+                    cell.detailTextLabel?.textColor = myPurple
+                }
+                
+            }
+        }
+        
+        return cell
+    }
+    
+    //What happens when a cell is tapped.
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if fridgeItems.count == 0 {
+            print("User tapped the empty cell message! Do nothing.")
+            tableView.deselectRow(at: indexPath, animated: true)
+            
+        } else {
+            
+            let fridgeItem = fridgeItems[indexPath.row]
+            performSegue(withIdentifier: "fridgeItemSegue", sender: fridgeItem)
+        }
+    }
+    
+    //Sets up the next ViewController (ItemViewController) and sends some item data over.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "fridgeItemSegue" {
+            
+            let backButton = UIBarButtonItem()
+            backButton.title = "Done"
+            navigationItem.backBarButtonItem = backButton
+            
+            let nextViewController = segue.destination as! FridgeItemViewController
+            nextViewController.fridgeItem = sender as? FridgeItem
+            
+            //Hide tab bar in item view.
+            nextViewController.hidesBottomBarWhenPushed = true
+            
+        } else {
+            
+            let nextViewController = segue.destination as! InfoViewController
+            nextViewController.navigationItem.title = "Info"
+            
+            //Hide tab bar in settings.
+            nextViewController.hidesBottomBarWhenPushed = true
+        }
+    }
+    
+    //Display/hide the table and empty message accordingly.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if fridgeItems.count == 0 {
+            itemListTableView.isHidden = true
+            emptyMessage1.isHidden = false
+            emptyMessage2.isHidden = false
+            
+        } else {
+            
+            emptyMessage1.isHidden = true
+            emptyMessage2.isHidden = true
+            itemListTableView.isHidden = false
+        }
+    }
+    
+    //Swipe actions (add to shopping list & delete).
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        //Swipe to add items to shopping list
+        
+        let swipeToAdd = UITableViewRowAction(style: .normal, title: "Shopping List") { (action:UITableViewRowAction!, NSIndexPath) in
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let SLItem = ShoppingListItem(context: context)
+            SLItem.name = self.fridgeItems[indexPath.row].name
+            SLItem.image = self.fridgeItems[indexPath.row].image!
+            
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            //Add badge to SL icon
+            
+            for item in self.tabBarController!.tabBar.items! {
+                if item.title == "Shopping List" {
+                    item.badgeValue = "New!"
+                }
+                
+            }
+            
+            //Create the alert when an item has been added to the shopping list.
+            
+            if (self.fridgeItems[indexPath.row].name?.isEmpty)! {
+                let alertVC = UIAlertController(title: "Item added", message: "This item has been added to your Shopping List.", preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+                
+                alertVC.addAction(okAction)
+                
+                self.present(alertVC, animated: true, completion: nil)
+                
+            } else {
+                
+                let alertVC = UIAlertController(title: "Item added", message: "\(self.fridgeItems[indexPath.row].name!) added to your Shopping List.", preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+                
+                alertVC.addAction(okAction)
+                
+                self.present(alertVC, animated: true, completion: nil)
+            }
+        }
+        
+        swipeToAdd.backgroundColor = .purple
+        
+        //Swipe to delete items from the Freezr.
+        
+        let swipeToDelete = UITableViewRowAction(style: .normal, title: "Delete") { (action:UITableViewRowAction!, NSIndexPath) in
+            
+            let fridgeItem = self.fridgeItems[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(fridgeItem)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do {
+                self.fridgeItems = try context.fetch(FridgeItem.fetchRequest())
+                tableView.reloadData()
+            } catch {}
+        }
+        
+        swipeToDelete.backgroundColor = UIColor.red
+        
+        return[swipeToDelete, swipeToAdd]
+    }
+    
+    //Final declaration:
+    
+}
